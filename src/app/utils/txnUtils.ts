0d8f320f51506bdacb25a40a312ce944e0e5a260
Agent 007 <pphiri1012x@gmail.com>
2022-10-24 20:02:56 +0200

    Initial commit

---
+++ src/app/utils/txnUtils.ts
@@ -0,0 +1,105 @@
+import {SignerTransaction} from "@perawallet/connect/dist/util/model/peraWalletModels";
+import algosdk from "algosdk";
+
+import algod from "./algod";
+
+async function generateOptIntoAssetTxns({
+  assetID,
+  initiatorAddr
+}: {
+  assetID: number;
+  initiatorAddr: string;
+}): Promise<SignerTransaction[]> {
+  const suggestedParams = await algod.getTransactionParams().do();
+  const optInTxn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+    from: initiatorAddr,
+    to: initiatorAddr,
+    assetIndex: assetID,
+    amount: 0,
+    suggestedParams
+  });
+
+  return [{txn: optInTxn, signers: [initiatorAddr]}];
+}
+
+async function generatePaymentTxns({
+  to,
+  initiatorAddr
+}: {
+  to: string;
+  initiatorAddr: string;
+}) {
+  const suggestedParams = await algod.getTransactionParams().do();
+
+  const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    from: initiatorAddr,
+    to,
+    amount: 1,
+    suggestedParams
+  });
+
+  return [{txn, signers: [initiatorAddr]}];
+}
+
+async function generateAssetTransferTxns({
+  to,
+  assetID,
+  initiatorAddr
+}: {
+  to: string;
+  assetID: number;
+  initiatorAddr: string;
+}) {
+  const suggestedParams = await algod.getTransactionParams().do();
+
+  const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+    from: initiatorAddr,
+    to,
+    assetIndex: assetID,
+    amount: 55000000,
+    suggestedParams
+  });
+
+  return [{txn, signers: [initiatorAddr]}];
+}
+
+async function generateLoanPaymentTxns({
+  initiatorAddr,
+  contract_id
+}: {
+  initiatorAddr: string;
+  contract_id: number;
+}) {
+  const op = "payment";
+
+  const args = [];
+  const accounts = [];
+
+  accounts.push("4EPIPODKXJKT7RDEANYFWILIW24TPUKOQMPEGGKSWIKCR5ULX2ARXZ6H3U");
+
+  const assets = [parseInt("10458941")];
+
+  args.push(new Uint8Array(Buffer.from(op)));
+  const params = await algod.getTransactionParams().do();
+
+  const txn = algosdk.makeApplicationNoOpTxn(
+    "F5TQCJVRAGQKYXKIGRLAP7NDWPPULK5FUT6TTUAFZBQAOK4WWHIHEWR6IU",
+    params,
+    contract_id,
+    args,
+    accounts,
+    [],
+    assets,
+    undefined,
+    undefined
+  );
+
+  return [{txn, signers: [initiatorAddr]}];
+}
+
+export {
+  generateOptIntoAssetTxns,
+  generatePaymentTxns,
+  generateAssetTransferTxns,
+  generateLoanPaymentTxns
+};
